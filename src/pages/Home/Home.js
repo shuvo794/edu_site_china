d635d98e7a05bbb99db5d0b61c9eecc65cd1ad24
shuvo794 <shuvohossain794@gmail.com>
2023-02-25 00:10:15 +0600

    work a initial feture

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "../Shared/Header/Header";
 import Banner from "../Banner/Banner";
+import Service from "../Service/Service";
 
 const Home = () => {
   return (
     <div>
       <Header />
       <Banner />
+      <Service />
     </div>
   );
 };
