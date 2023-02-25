e7bf1bd6bf5fce22467f928f16405d44b465ceb6
shuvo794 <shuvohossain794@gmail.com>
2023-02-25 18:45:03 +0600

    all fix

--- src/pages/Fly/Fly.js
+++ src/pages/Fly/Fly.js
@@ -6,29 +6,27 @@ const Fly = () => {
     <div className="container">
       <div className="row ">
         <div className="col-lg-4 col-md-6 d-flex align-items-stretch">
-          <img className="desti" height="360px" src={destination} alt="" />
+          <img width="auto" height="100%" src={destination} alt="" />
         </div>
-        <div className="col-lg-8 col-md-6 d-flex align-items-stretch">
+        <div className="col-lg-8 col-md-6  d-flex align-items-stretch">
           <div className="bg1">
-            <br />
-            <br />
+            <div className="section-title px-5 mb-5">
+              <div className="visa-text">
+                <h3 className="text-white mb-3">Fly Your Dream Destination</h3>
 
-            <div className="section-title  mb-5">
-              <h3 className="text-white mb-8">Fly Your Dream Destination</h3>
-              <br></br>
-              <h5 className="idea">
-                Idea of denouncing pleasure & praising pain was born.
-              </h5>
-              <p className="text-white fw-700 pera">
-                Entering & Leaving From Country
-              </p>
-              <p className="text-white fw-700 pera2">Visas</p>
-              <p className="text-white fw-700 pera3">Country Citizenship</p>
-              <input
-                className="submit-btn btn btn-primary pera4"
-                type="submit"
-                value="Contact Us"
-              />
+                <h5 className="text-white">
+                  Idea of denouncing pleasure & praising pain was born.
+                </h5>
+                <p className="text-white fw-700 pera">
+                  Entering & Leaving From Country
+                </p>
+
+                <input
+                  className="submit-btn btn btn-primary pera4"
+                  type="submit"
+                  value="Contact Us"
+                />
+              </div>
             </div>
           </div>
         </div>
